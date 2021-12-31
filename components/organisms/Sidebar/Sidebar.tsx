a4c12c8154acea65605495d0db0c06f0e9bf1010
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2021-12-31 19:10:09 +0100

    New component Sidebar

---
+++ components/organisms/Sidebar/Sidebar.tsx
@@ -0,0 +1,21 @@
+import MenuSection from '../../molecules/MenuSection/MenuSection';
+import { menu } from '../../../menu/menu';
+import Link from 'next/link';
+const Sidebar = () => {  
+  return (
+    <nav id="sidebar_wrapper">
+            <Link href="/">
+               <a className="menuSectionHeader">HomePage</a>
+            </Link> 
+            <MenuSection sectionName="Data Science" sectionLinks={menu.dataScience} />
+            <MenuSection sectionName="Kategorie" sectionLinks={menu.categories} />
+            <MenuSection sectionName="Serie" sectionLinks={menu.serie} />
+            <Link href="/contact">
+               <a className="menuSectionHeader">Contact</a>
+            </Link> 
+            <MenuSection sectionName="Admin" sectionLinks={menu.admin} />
+    </nav>
+  );
+};
+
+export default Sidebar;

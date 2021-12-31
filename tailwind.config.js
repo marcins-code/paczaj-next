74cbf2183cadd0eb84a1a9fa251e386e27948b63
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2021-12-31 19:09:00 +0100

    Styles extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'dark-bg-image': 'url("https://www.transparenttextures.com/patterns/dirty-old-black-shirt.png")',
+        'light-bg-image': 'url("https://www.transparenttextures.com/patterns/first-aid-kit.png")',
+        // '': "url('/img/footer-texture.png')",
+        
+      },
+      backgroundColor: {
+        'dark-bg-color': "rgba(10,10,10,0.93)",
+        'light-bg-color': "rgba(210,210,210,0.93)",
+      }
+    
+       
+    },
+    fontFamily: {
+      'sans': ['"Open Sans"'],
+    }
   },
-  plugins: [],
+  plugins: [require('tailwindcss-textshadow')],
+  darkMode: 'class', 
 };

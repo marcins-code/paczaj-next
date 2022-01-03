9ba8b12e41a7b901247c7bb72c69dd5719d51941
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-01-03 03:04:51 +0100

    Cleaning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import MainLayout from '../layouts/MainLayout';
-import axios from 'axios'
+
 
 const MyApp = ({ Component, pageProps }: AppProps) => {  
   return (

293ab46109106494874df74085feab0820484c4b
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-01-05 14:05:09 +0100

    New Api file

--- pages/admin/article-types/[ArticleTypeId].tsx
+++ pages/admin/article-types/[ArticleTypeId].tsx
@@ -8,7 +8,9 @@ import { html } from '@codemirror/lang-html';
 import { oneDark } from '@codemirror/theme-one-dark';
 import DB from '../../../db/db';
 import { ObjectId } from 'mongodb';
-import {menuItems, menuItemsDecriptions} from '../../../utils/menu';
+import { menuItems, menuItemsDecriptions } from '../../../utils/menu';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 interface MyFormValues {
   name: string;
@@ -18,18 +20,15 @@ interface MyFormValues {
 }
 
 const ArticleTypeId = (props: any) => {
-  console.log(props);
-
   const router = useRouter();
-  const [description, setDescription] = useState(props.description.pl);
+  const [description, setDescription] = useState(props.description ? props.description.pl : '');
   const { ArticleTypeId } = router.query;
-  const initialValues: MyFormValues = { 
-    
-    name: props.name, 
-    icon: props.icon, 
-    type: props.type || undefined, 
-    isEnabled: props.isEnabled || false };
-  // console.log(description);
+  const initialValues: MyFormValues = {
+    name: props.name,
+    icon: props.icon,
+    type: props.type || undefined,
+    isEnabled: props.isEnabled || false
+  };
 
   const SignupSchema = Yup.object().shape({
     name: Yup.string()
@@ -59,8 +58,31 @@ const ArticleTypeId = (props: any) => {
               en: ''
             }
           };
+          const method = ArticleTypeId === 'new' ? 'post' : 'put';
+          const url =
+            ArticleTypeId === 'new'
+              ? '/api/article-types/new'
+              : '/api/article-types/' + ArticleTypeId;
+          axios({
+            method: method,
+            url: url,
+            data: body
+          })
+            .then((response) => {
+              if (response.status === 201) {
+                router.push('/admin/article-types/' + response.data.insertedId);
+                toast.success('Utworzono nowy typ artykułu !');
+              }
 
-          console.log(body);
+              if (response.status === 200) {
+                toast.success('Zapisano zmiany!');
+              }
+
+            })
+            .catch((error) => {
+              toast.error(error.response.data.message);
+         
+            });
         }}>
         {({ errors, touched, values }) => (
           <Form>
@@ -95,8 +117,10 @@ const ArticleTypeId = (props: any) => {
                   as="select"
                   className="dark:text-zinc-200 dark:bg-neutral-700 h-10 px-1 focus:outline-none">
                   <option value="">--wybierz--</option>
-                  {menuItems.map((item)=>(
-                    <option value={item} key={item}>{menuItemsDecriptions[item as 'dataScience' | 'developmnet' | 'serie']}</option>
+                  {menuItems.map((item) => (
+                    <option value={item} key={item}>
+                      {menuItemsDecriptions[item as 'dataScience' | 'developmnet' | 'serie']}
+                    </option>
                   ))}
                   <option value="red">Red</option>
                   <option value="green">Green</option>
@@ -113,8 +137,7 @@ const ArticleTypeId = (props: any) => {
               </div>
             </div>
             <CodeMirror
-              value={props.description.pl}
-              // value="console.log('hello world!');"
+              value={description}
               height="auto"
               theme={oneDark}
               extensions={[html({})]}
@@ -131,20 +154,27 @@ const ArticleTypeId = (props: any) => {
 };
 
 export async function getServerSideProps(context: any) {
+  let result;
   const { ArticleTypeId } = context.query;
-  const db = await DB.setUp();
-  const ATCollection = db.collection('articleTypes');
-  const result = await ATCollection.findOne({ _id: new ObjectId(ArticleTypeId) });
-  console.log(result);
 
-  return {
-    props: {
-      name: result ? result.name : '',
-      icon: result ? result.icon : '',
-      type: result ? result.type : '',
-      isEnabled: result ? result.isEnabled : false,
-      description: result ? result.description : false
-    }
-  };
+  if (ArticleTypeId === 'new') {
+    return { props: {} };
+  } else {
+    // if (ArticleTypeId !== 'new '){
+    const db = await DB.setUp();
+    const ATCollection = db.collection('articleTypes');
+    const result = await ATCollection.findOne({ _id: new ObjectId(ArticleTypeId) });
+
+    return {
+      props: {
+        name: result ? result.name : '',
+        icon: result ? result.icon : '',
+        type: result ? result.type : '',
+        isEnabled: result ? result.isEnabled : false,
+        description: result ? result.description : false
+      }
+    };
+  }
 }
+
 export default ArticleTypeId;

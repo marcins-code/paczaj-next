ad62dc630ae941a64eeda937b3a01e045e7bff65
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-01-05 14:03:12 +0100

    Add ToastContainer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,28 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import MainLayout from '../layouts/MainLayout';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {  
+const MyApp = ({ Component, pageProps }: AppProps) => {
+
   return (
     <MainLayout>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='colored'
+      />
       <Component {...pageProps} />
     </MainLayout>
   );
-}
+};
 
 export default MyApp;
